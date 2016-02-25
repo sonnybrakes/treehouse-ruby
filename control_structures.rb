265907ddaac3540a66f77a4e1e050109e4dc9a84
Michel Frechette <mifrec@hotmail.com>
2016-02-25 07:47:50 -0500

    if statements

---
+++ control_structures.rb
@@ -0,0 +1,12 @@
+car1_speed = 500
+car2_speed = 300
+
+if car1_speed > car2_speed
+  puts "Car 1 is faster than Car 2"
+end
+
+car2_speed += 300
+
+if car2_speed > car1_speed
+  puts "Car 2 is faster than Car 1"
+end
