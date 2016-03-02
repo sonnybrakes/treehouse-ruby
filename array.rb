8491fccd49a655b55c17e08a15c049ece2e4f2d6
Michel Frechette <mifrec@hotmail.com>
2016-03-01 19:22:02 -0500

    accessing items in an array

--- array.rb
+++ array.rb
@@ -1,3 +1,26 @@
-puts grocery_list = %w(milk eggs cats)
-grocery_list << 'cows'
+# puts grocery_list = %w(milk eggs cats)
+# grocery_list << 'cows' #adds item to the end of the array
+# puts grocery_list
+# grocery_list.push('pot')
+# puts grocery_list
+# grocery_list += ['hats']
+# puts grocery_list
+# grocery_list.unshift('first') #adds item to the beginning of the array
+# puts grocery_list
+# grocery_list << 1
+# puts grocery_list
+grocery_list = %w(milk eggs bread ice-cream pie potatoes)
 puts grocery_list
+puts grocery_list[0]
+puts grocery_list.at(1)
+item = grocery_list.at(2)
+puts item
+puts grocery_list.first
+puts grocery_list.last
+puts grocery_list[-1]
+puts grocery_list.insert(2, 'toast')
+puts grocery_list.length
+puts grocery_list.count('eggs')
+puts grocery_list.push('eggs')
+puts grocery_list.count('eggs')
+puts grocery_list.include?('eggs')
